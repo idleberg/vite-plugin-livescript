aac846c8601e769d3bde7a33118a56aeaa42b82c
Jan T. Sott <git@idleberg.com>
2023-09-24 14:29:10 +0200

    format

--- tests/vite-javascript.config.js
+++ tests/vite-javascript.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import livescript from '../dist/index.js'
 
 export default defineConfig({
-    plugins: [
-        livescript()
-    ],
-    build: {
-        emptyOutDir: false,
-        rollupOptions: {
-            input: './fixtures/js-entrypoint.js'
-        }
-    }
+		plugins: [
+				livescript()
+		],
+		build: {
+				emptyOutDir: false,
+				rollupOptions: {
+						input: './fixtures/js-entrypoint.js'
+				}
+		}
 });
--- tests/vite-typescript.config.js
+++ tests/vite-typescript.config.js
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite'
 import livescript from '../dist/index.js'
 
 export default defineConfig({
-    plugins: [
-        livescript()
-    ],
-    build: {
-        emptyOutDir: false,
-        rollupOptions: {
-            input: './fixtures/ts-entrypoint.ts'
-        }
-    }
+		plugins: [
+				livescript()
+		],
+		build: {
+				emptyOutDir: false,
+				rollupOptions: {
+						input: './fixtures/ts-entrypoint.ts'
+				}
+		}
 });
